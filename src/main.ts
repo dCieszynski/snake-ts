c6c12b858011c3874fa3d62f4a77e3a8ad8edbaf
dCieszynski <dCieszynski99@gmail.com>
2022-08-06 06:20:42 -0500

    Rendering snake

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 const gameScreen = document.querySelector(".game__screen");
+const snakeHeadPos = 41;
+const snakeTailPos = snakeHeadPos;
 
 const levelWidth: number = 9;
 const levelHeight: number = 9;
 const levelSize = levelWidth * levelHeight;
 
 const renderLevel = () => {
-  for (let i = 0; i < levelSize; i++) {
+  for (let i = 1; i <= levelSize; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
+    cell.setAttribute("id", `cell_${i}`);
     gameScreen?.appendChild(cell);
   }
+
+  renderSnake();
+};
+
+const renderSnake = () => {
+  const headCell = document.querySelector(`#cell_${snakeHeadPos}`);
+  const tailCell = document.querySelector(`#cell_${snakeTailPos}`);
+  headCell?.classList.add("snake");
+  tailCell?.classList.add("snake");
 };
 
 renderLevel();
