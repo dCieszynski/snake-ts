1bef71cfee24c5c057a3c213040f97645275f7d0
dCieszynski <dCieszynski99@gmail.com>
2022-08-08 19:02:57 -0500

    Ntl deploy

--- deploy.sh
+++ deploy.sh
@@ -44,12 +44,12 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -b main
+git checkout main
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+#git push -f -u git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:dCieszynski/ts-snake.git main:gh-pages
